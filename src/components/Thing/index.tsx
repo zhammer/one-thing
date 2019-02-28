bb1b422d7705f316dff18e50c6c364f2529576ac
zhammer <zachary_hammer@alumni.brown.edu>
2019-02-27 20:13:31 -0500

    Add contact info to thing

--- src/components/Thing/index.tsx
+++ src/components/Thing/index.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import "./Thing.scss";
 import Checkmark from "../Checkmark";
+import Email from "../Email";
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
 
 interface ThingProps {
   text: string;
   complete?: boolean;
+  person: Person;
 }
 
-export default function Thing({ text, complete = false }: ThingProps) {
+export default function Thing({ text, complete = false, person }: ThingProps) {
   return (
     <div className={`thing thing--${complete ? "complete" : "noncomplete"}`}>
-      <div className="thing__text">{text}</div>
-      {complete && <Checkmark className='thing__checkmark' />}
+      <div className="thing__body">
+        <div className="thing__text">{text}</div>
+        <div className="thing__contact">
+          {makePersonName(person)} <Email className='email-icon' />
+        </div>
+      </div>
+      {complete && <Checkmark className="thing__checkmark" />}
     </div>
   );
+}
+
+function makePersonName(person: Person) {
+  return person.firstName + ' ' + person.lastName.charAt(0);
 }
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -15,22 +15,40 @@ export default function LoginPage() {
         <span className="seatgeek-blue">SeatGeek</span>.
       </Subtitle>
       <div className="example-container">
-        <Thing text="Clean up 10 old tickets on gitlab." complete />
         <Thing
-          text="Chat with someone who's given an OKR presentation to get some tips for my first OKR presentation tomorrow."
+          text="Clean up 10 old tickets on gitlab."
+          person={people[0]}
           complete
         />
         <Thing
+          text="Chat with someone who's given an OKR presentation to get some tips for my first OKR presentation tomorrow."
+          person={people[1]}
+          complete
+        />
+        <Thing
+          person={people[2]}
           text="Enjoy the nice weather with a coworker for lunch at washington square park."
         />
-        <Thing text="Learn what PRISM is. (It's my first week!)"/>
         <Thing
+          person={people[3]}
+          text="Learn what PRISM is. (It's my first week!)"
+        />
+        <Thing
+          person={people[4]}
           text="Figure out what caused several SGO transfers to fail over the weekend."
         />
       </div>
-      <div className='button-container'>
-        <Button type='primary'>Sign in with Gmail</Button>
+      <div className="button-container">
+        <Button type="primary">Sign in with Gmail</Button>
       </div>
     </Page>
   );
 }
+
+const people = [
+  { firstName: "Rufus", lastName: "SeatGeek" },
+  { firstName: "Simone", lastName: "Biles" },
+  { firstName: "Peyton", lastName: "Manning" },
+  { firstName: "Janis", lastName: "Joplin" },
+  { firstName: "Zacarias", lastName: "Ferreira" }
+];
