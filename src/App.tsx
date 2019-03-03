aa01e606ceb6fa33f22736a9ceb1673b8fba94e8
zhammer <zachary_hammer@alumni.brown.edu>
2019-03-03 13:17:52 -0500

    Create top-level styles directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import LoginPage from './pages/LoginPage';
 import { ThemeProvider } from './custom/styled-components';
-import theme from './theme';
+import theme from './styles/theme';
 import { GlobalStyle } from './App.styles';
 
 export default function App() {
