1f84d92d7995eded4bacc6caa4eaa08266c9ac36
zhammer <zachary_hammer@alumni.brown.edu>
2019-03-07 21:17:18 -0500

    Only show 3 examples on mobile

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -4,10 +4,12 @@ import Title from "../../components/Title";
 import Subtitle from "../../components/Subtitle";
 import Thing from "../../components/Thing";
 import Button from "../../components/Button";
+import { isMobile } from "react-device-detect";
 import { ExampleContainer, ButtonContainer, SeatGeekBlue } from "./LoginPage.styles";
 import { ThingInterface } from "../../types";
 
 export default function LoginPage() {
+  const things = isMobile ? mobileThings : allThings;
   return (
     <Page.WithoutNav>
       <Title>One Thing</Title>
@@ -27,30 +29,35 @@ export default function LoginPage() {
   );
 }
 
-const things: ThingInterface[] = [
+const allThings: ThingInterface[] = [
   {
     description: "Clean up 10 old tickets on gitlab.",
     person: { firstName: "Rufus", lastName: "SeatGeek" },
-    complete: true,
+    complete: true
   },
   {
-    description: "Chat with someone who's given an OKR presentation to get some tips for my first OKR presentation.",
+    description:
+      "Chat with someone who's given an OKR presentation to get some tips for my first OKR presentation.",
     person: { firstName: "Simone", lastName: "Biles" },
-    complete: true,
+    complete: true
   },
   {
-    description: "Enjoy the nice weather with a coworker for lunch at washington square park.",
+    description:
+      "Enjoy the nice weather with a coworker for lunch at washington square park.",
     person: { firstName: "Peyton", lastName: "Manning" },
-    complete: false,
+    complete: false
   },
   {
     description: "Learn what PRISM is. (It's my first month!)",
     person: { firstName: "Janis", lastName: "Joplin" },
-    complete: false,
+    complete: false
   },
   {
-    description: "Figure out what caused several SGO transfers to fail over the weekend.",
+    description:
+      "Figure out what caused several SGO transfers to fail over the weekend.",
     person: { firstName: "Zacarias", lastName: "Ferreira" },
-    complete: false,
-  },
+    complete: false
+  }
 ];
+
+const mobileThings = [allThings[0], allThings[3], allThings[4]];
