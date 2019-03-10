bfa5a94737e36141fd7b1a1891ed8191c29f9286
zhammer <zachary_hammer@alumni.brown.edu>
2019-03-10 11:29:04 -0400

    Add settings page spec

---
+++ cypress/integration/SettingsPage/steps.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
+
+beforeEach(() => {
+  cy.clearLocalStorage();
+});
+
+Given("I am logged in", () => {
+  localStorage.setItem("accessToken", "cypress_access_token");
+});
+
+Given("I am on the settings page", () => {
+  cy.visit("/settings");
+});
+
+When(`I click the button that says {string}`, buttonText => {
+  cy.get("button")
+    .contains(buttonText)
+    .click();
+});
+
+Then("I am redirected to the login page", () => {
+  cy.location("pathname").should("eq", "/login");
+});
+
+Then("my login information is gone", () => {
+  cy.then(() => {
+    expect(localStorage.getItem("accessToken")).to.be.null;
+  })
+});
