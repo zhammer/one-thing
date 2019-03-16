75f0ec7465ed9d1eb47de846be23170df0d485fc
zhammer <zachary_hammer@alumni.brown.edu>
2019-03-16 19:11:28 -0400

    Add SeatGeekPage feature

---
+++ cypress/integration/SeatGeekPage.feature
@@ -0,0 +1,24 @@
+Feature: SeatGeek Page
+  On the SeatGeek page I can see the things that people at SeatGeek
+  want to do this week.
+
+  Background:
+    Given I am logged in
+
+  Scenario: Nobody at SeatGeek has submitted a Thing this week
+    Given nobody at SeatGeek has submitted a Thing this week
+    When I visit the SeatGeek page
+    Then I see the title "SeatGeek"
+    And I see the subtitle "Be the first person to submit your One Thing this week!"
+
+  Scenario: People at Seatgeek have submitted Things this week
+    Given the following things have been submitted this week:
+      | id | firstName | lastName | description                | complete | createdAt           |
+      | 1  | Zach      | Hammer   | Learn about interviewing.  | true     | 2019-03-16T00:10:15 |
+      | 2  | Billie    | Holiday  | Go to a #music-club event. | false    | 2019-03-17T00:11:00 |
+      | 3  | Lebron    | James    | Join a SG sports team.     | true     | 2019-03-18T05:42:12 |
+    When I visit the SeatGeek page
+    Then I see the title "SeatGeek"
+    And I see the subtitle "Here are the things people at SeatGeek want to do this week!"
+    And I see all the submitted things
+
---
+++ cypress/integration/SeatGeekPage/steps.js
@@ -0,0 +1,77 @@
+/// <reference types="cypress" />
+
+import { Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
+
+beforeEach(() => {
+  cy.server();
+  cy.mockGraphql({ schema: Cypress.env('GRAPHQL_SCHEMA') });
+});
+
+Given('nobody at SeatGeek has submitted a Thing this week', () => {
+  cy.mockGraphqlOps({
+    operations: {
+      SeatGeekThingsThisWeek: {
+        seatGeekThingsThisWeek: {
+          edges: []
+        }
+      }
+    }
+  });
+});
+
+Given('the following things have been submitted this week:', dataTable => {
+  const things = pluckThings(dataTable);
+  cy.wrap(things).as('things');
+  cy.mockGraphqlOps({
+    operations: {
+      SeatGeekThingsThisWeek: {
+        seatGeekThingsThisWeek: {
+          edges: buildEdges(things)
+        }
+      }
+    }
+  });
+});
+
+When('I visit the SeatGeek page', () => {
+  cy.visit('/seatgeek');
+});
+
+Then('I see all the submitted things', () => {
+  cy.get('@things').then(things => {
+    cy.get('div[data-class-name=thing-container]')
+      .children()
+      .should('have.length', things.length)
+      .each((thingComponent, i) => {
+        const thing = things[i];
+        expect(thingComponent).to.contain(thing.description);
+        expect(thingComponent).to.contain(
+          'a',
+          thing.person.firstName + ' ' + thing.person.lastName.charAt(0)
+        );
+        cy.wrap(thingComponent)
+          .find('svg[data-class-name=icon-checkmark]')
+          .should(thing.complete ? 'exist' : 'not.exist');
+      });
+  });
+});
+
+function pluckThings(rawTable) {
+  return rawTable.hashes().map(rawThing => ({
+    id: rawThing.id,
+    description: rawThing.description,
+    complete: rawThing.complete === 'true',
+    createdAt: rawThing.createdAt,
+    person: {
+      firstName: rawThing.firstName,
+      lastName: rawThing.lastName
+    }
+  }));
+}
+
+function buildEdges(things) {
+  return things.map(thing => ({
+    cursor: thing.id,
+    node: thing
+  }));
+}
