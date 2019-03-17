53c0b3ac17d6780768f66db8a02fd94d79877b81
zhammer <zachary_hammer@alumni.brown.edu>
2019-03-17 11:53:00 -0400

    Add cypress-app-watcher-preprocessor
    Now in dev mode if the react app hot relods, cypress tests will
    restart as well.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,10 +1,11 @@
 const cucumber = require('cypress-cucumber-preprocessor').default
 const fs = require('fs');
+const watchApp = require('cypress-app-watcher-preprocessor');
 
 const graphQlSchema = fs.readFileSync('schema.graphql', 'utf8');
 
 module.exports = (on, config) => {
-  on('file:preprocessor', cucumber())
+  on('file:preprocessor', watchApp(cucumber()));
 
   config.env.GRAPHQL_SCHEMA = graphQlSchema;
   return config;
