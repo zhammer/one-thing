67c33eb1d27e9a34725798890d3f32476f00390a
zhammer <zachary_hammer@alumni.brown.edu>
2019-04-08 14:53:07 -0400

    Use client credentials flow for obtaining access key to auth0 api

--- one_thing/src/gateways/auth0Gateway.ts
+++ one_thing/src/gateways/auth0Gateway.ts
@@ -3,17 +3,30 @@ import request from 'superagent';
 import { Auth0Error } from '../exceptions';
 
 export default class Auth0Gateway implements Auth0GatewayInterface {
-  private url: string = 'https://zhammer.auth0.com/api/v2';
-  private accessToken: string;
+  private readonly base: string = 'https://zhammer.auth0.com';
+  private readonly clientId: string;
+  private readonly clientSecret: string;
 
-  constructor(accessToken: string) {
-    this.accessToken = accessToken;
+  constructor(clientId: string, clientSecret: string) {
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+  }
+
+  private async fetchAccessToken(): Promise<string> {
+    const response = await request.post(`${this.base}/oauth/token`).send({
+      grant_type: 'client_credentials',
+      client_id: this.clientId,
+      client_secret: this.clientSecret,
+      audience: 'https://zhammer.auth0.com/api/v2/'
+    });
+    return response.body.access_token;
   }
 
   async fetchUserInfo(auth0UserId: string): Promise<UserInfo> {
+    const accessToken = await this.fetchAccessToken();
     const response = await request
-      .get(`${this.url}/users/${auth0UserId}`)
-      .set('Authorization', `Bearer ${this.accessToken}`);
+      .get(`${this.base}/api/v2/users/${auth0UserId}`)
+      .set('Authorization', `Bearer ${accessToken}`);
     if (response.status !== 200) {
       throw new Auth0Error();
     }
