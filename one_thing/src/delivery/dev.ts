47961fb2892baccaa8dc857eeb4d1d04950fa347
zhammer <zachary_hammer@alumni.brown.edu>
2019-04-11 13:22:01 -0400

    Pass in config to makeApolloServer instead of getting from env

--- one_thing/src/delivery/dev.ts
+++ one_thing/src/delivery/dev.ts
@@ -1,6 +1,13 @@
 import { makeApolloServer } from './apollo';
 
+const auth0ClientId = process.env.AUTH0_CLIENT_ID || 'auth0_client_id';
+const auth0ClientSecret = process.env.AUTH0_CLIENT_SECRET || 'auth0_client_secret';
+const postgresUrl = process.env.DATABASE_CONNECTION_STRING;
+
 const server = makeApolloServer({
+  auth0ClientId,
+  auth0ClientSecret,
+  postgresUrl,
   dev: true
 });
 server.listen(4000).then(({ url }) => console.log(`Server ready at ${url}`));
