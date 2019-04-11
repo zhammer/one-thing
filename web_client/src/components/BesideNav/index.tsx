3a33e7aa31d5289246eeed6321ab975b27cd2668
zhammer <zachary_hammer@alumni.brown.edu>
2019-04-11 13:20:36 -0400

    Add padding for BesideNav for mobile

--- web_client/src/components/BesideNav/index.tsx
+++ web_client/src/components/BesideNav/index.tsx
@@ -1,7 +1,13 @@
 import styled from '../../custom/styled-components';
-import { mobileBreakpoint, navbarWidth } from '../../styles/variables';
+import {
+  mobileBreakpoint,
+  navbarWidth,
+  mobileNavbarHeight
+} from '../../styles/variables';
 
 const BesideNav = styled.div`
+  padding: 0 0 ${mobileNavbarHeight} 0;
+
   @media screen and (min-width: ${mobileBreakpoint}) {
     padding: 0 0 0 ${navbarWidth};
   }
