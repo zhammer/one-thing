e6e6e83decff625d974b257c5e247ad76cd7d812
zhammer <zachary_hammer@alumni.brown.edu>
2019-04-13 01:14:32 -0400

    Add more util functions

--- web_client/src/util.ts
+++ web_client/src/util.ts
@@ -1,3 +1,19 @@
 export function getRandomItem<T>(items: T[]): T {
   return items[Math.floor(Math.random() * items.length)];
 }
+
+export function randomWholeNumber(min: number, max: number) {
+  return min + Math.floor(Math.random() * (max - min));
+}
+
+/**
+ * range(4) -> [0, 1, 2, 3]
+ * @param count Length of range
+ */
+export function range(count: number): number[] {
+  let out: number[] = [];
+  for (let i = 0; i < count; i++) {
+    out.push(i);
+  }
+  return out;
+}
