fb619f19193a36f32f6d2a50776da958d5640950
Zach Hammer <zachary_hammer@alumni.brown.edu>
2019-04-14 19:29:49 -0400

    Webpack prod config (#38)
    * Setup webpack prod config
    
    * Rename dev.ts to server.ts
    
    * Prettify webpack.development.js
    
    * Don't minimize
    
    Webpack 4 default minimizes code. For some reason that's causing a
    cyclic dependency on production builds specifically when I try to
    submit a new thing (after having submitted one last week). I believe
    it has to do with typeorm but really am not sure.

--- one_thing/webpack.production.js
+++ one_thing/webpack.production.js
@@ -1,14 +1,17 @@
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const merge = require("webpack-merge");
-const nodeExternals = require("webpack-node-externals");
-const path = require("path");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const merge = require('webpack-merge');
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
 
-const common = require("./webpack.common.js");
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  devtool: "source-map",
-  entry: [path.join(__dirname, "src/main.ts")],
+  devtool: 'source-map',
+  entry: [path.join(__dirname, 'src/delivery/server.ts')],
   externals: [nodeExternals({})],
-  mode: "production",
-  plugins: [new CleanWebpackPlugin()]
+  mode: 'production',
+  plugins: [new CleanWebpackPlugin()],
+  optimization: {
+    minimize: false
+  }
 });
